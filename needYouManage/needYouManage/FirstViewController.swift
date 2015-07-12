e0da126ae22966049583e508c4db7101ab53752d
林青 吕 <671696763@qq.com>
2015-07-12 21:54:33 +0800

    init
    project init

---
+++ needYouManage/needYouManage/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  needYouManage
+//
+//  Created by 林青 吕 on 15/7/12.
+//  Copyright (c) 2015年 needYouManage. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
